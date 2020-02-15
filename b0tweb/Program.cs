5e576eca7e1e4b77fe6a3b118e3357911b2de501
J.M. Thiessen <jacob@x0rz3q.com>
2020-02-15 15:10:03 +0100

    First attempt of having a stable tor connection
    Connect via the binary itself

--- b0tweb/Program.cs
+++ b0tweb/Program.cs
@@ -10,13 +10,24 @@ namespace b0tweb
     /// </summary>
     class Program
     {
+        private const string Channel = "#b0tweb";
+
         /// <summary>
         /// Default main function to start the program.
         /// </summary>
         /// <param name="args">The command line arguments.</param>
         public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the b0tweb");
 
+            TorProxy proxy = new TorProxy();
+            proxy.Establish();
+
+            IRCConnectionController controller = new IRCConnectionController();
+            controller.Join(Program.Channel);
+            controller.Listen();
+
+            proxy.Disconnect();
         }
     }
 }
