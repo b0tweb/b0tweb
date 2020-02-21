96ba789b51e6c6a4098b335e009802787b3ee70f
JurisMajors <j.a.majors@student.tue.nl>
2020-02-21 10:40:58 +0100

    Add the kill switch handler

---
+++ b0tweb/MessageHandlers/KIllSwitchHandler.cs
@@ -0,0 +1,25 @@
+﻿using Meebey.SmartIrc4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace b0tweb.MessageHandlers
+{
+    class KillSwitchHandler : AbstractMessageHandler
+    {
+        /// <summary>
+        /// Key word that should be part of the message to trigger the kill switch
+        /// </summary>
+        const string KillKeyword = "life is suffering";
+        protected override void BuildHandler(object sender, IrcEventArgs e)
+        {
+            if (e.Data.Message.IndexOf(KillKeyword) != -1)
+            {
+                e.Data.Irc.RfcDie();
+                e.Data.Irc.Disconnect();
+            }
+            
+        }
+    }
+}
