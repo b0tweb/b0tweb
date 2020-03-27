c440386d2dc1ace3fb0a4de367e4d5acf17a7740
J.M. Thiessen <jacob@x0rz3q.com>
2020-03-27 11:56:57 +0100

    Ignore IRC messages that are not meant for the current user

--- b0tweb/MessageHandlers/CommandMessageHandler.cs
+++ b0tweb/MessageHandlers/CommandMessageHandler.cs
@@ -5,10 +5,27 @@ namespace b0tweb.MessageHandlers
 {
     class CommandMessageHandler : AbstractMessageHandler
     {
+        /// <summary>
+        /// The nickname of the current IRC bot user.
+        /// </summary>
+        private string _nick;
+
+        public CommandMessageHandler(string nick)
+        {
+            this._nick = nick;
+        }
+
         protected override void BuildHandler(object sender, IrcEventArgs e)
         {
-            string response = CommandRegistry.ExecuteSync(e.Data.Message);
+            string[] parts = e.Data.Message.Split(' ');
 
+            //match *, nick {message} and nick: {message}
+            if (parts[0] != "*" && parts[0] != this._nick && parts[0] != this._nick + ":")
+            {
+                return;
+            }
+
+            string response = CommandRegistry.ExecuteSync(e.Data.Message);
             if (e.Data.Irc.JoinedChannels.Count > 0)
             {
                 e.Data.Irc.SendMessage(SendType.Message, e.Data.Irc.JoinedChannels[0], response);
